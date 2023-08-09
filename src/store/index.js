0b5635b6afa59a294e22b2761305d79510edd671
Edgar Rossi <edgarlrossi@gmail.com>
2023-08-09 20:44:11 -0300

    Creacion de store con vuex

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { store } from "quasar/wrappers";
+import { createStore } from "vuex";
+
+export default store(function () {
+  const Store = createStore({
+    modules: {},
+    strict: process.env.DEBUGGING,
+  });
+
+  return Store;
+});
